ab50d4fce74a681300ee150ef4138edd8a8ed6cf
Sumaiya <sumaiya7619@gmail.com>
2024-03-16 15:54:26 +0600

    Basic setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Blogs from './blog'
+import Header from './components/header/header'
+
+function App() {
+  return (
+    <> 
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/header.jsx
@@ -0,0 +1,8 @@
+const Header = () =>{
+    return (
+        <div>
+            <h1 className='text-4xl font-bold text-green-700 text-center'>Knowledge Cafe</h1>        </div>
+    )
+}
+
+export default Header;
